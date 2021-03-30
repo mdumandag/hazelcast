d72c194f177c845181964b77de70976fd152b066
sancar <mustafa.sancar.koyunlu@gmail.com>
2021-03-30 16:36:01 +0300

    restore SplitBrain and SccheduledExecutor commons

--- hazelcast/src/main/java/com/hazelcast/spi/merge/SplitBrainMergeTypes.java
+++ hazelcast/src/main/java/com/hazelcast/spi/merge/SplitBrainMergeTypes.java
@@ -25,6 +25,7 @@ import com.hazelcast.cp.IAtomicReference;
 import com.hazelcast.map.IMap;
 import com.hazelcast.multimap.MultiMap;
 import com.hazelcast.replicatedmap.ReplicatedMap;
+import com.hazelcast.scheduledexecutor.impl.ScheduledTaskDescriptor;
 
 import java.util.Collection;
 
@@ -128,6 +129,14 @@ public class SplitBrainMergeTypes {
      * @since 3.10
      */
     public interface AtomicReferenceMergeTypes extends MergingValue<Object> {
+    }
+
+    /**
+     * Provided merge types of {@link com.hazelcast.scheduledexecutor.IScheduledExecutorService}.
+     *
+     * @since 3.10
+     */
+    public interface ScheduledExecutorMergeTypes extends MergingEntry<String, ScheduledTaskDescriptor> {
     }
 
     /**
